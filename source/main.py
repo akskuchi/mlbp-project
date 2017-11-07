0074575b559c381bbfe9bb9fb91e0e21d8b381a9
Tommi Vehviläinen <tommi.vehvilainen@aalto.fi>
2017-11-07 12:13:16 +0200

    added logloss scoring (is it correct?)

--- source/main.py
+++ source/main.py
@@ -12,7 +12,7 @@ resource_path = os.path.join(
 paths = [os.path.join(resource_path, p) for p in
          ['train_data.csv', 'train_labels.csv', 'test_data.csv']]
 
-X, y, test_data = [pd.read_csv(p, header=None)
+X, y, target = [pd.read_csv(p, header=None)
                    for p in paths]
 
 X = X.values
@@ -21,22 +21,35 @@ y = y.values.ravel()
 pl = pipeline.Pipeline(
     [('scaler', preprocessing.StandardScaler()),
      # at the moment the pipeline performs better without
-     # PCA but is left here for reference. For optimal performance,
+     # PCA but it is left here for reference. For optimal performance
      # comment the PCA line before running the code.
      ('PCA', decomposition.PCA(n_components=0.8, svd_solver='full')),
-     ('classifier', linear_model.LogisticRegression(random_state=0))])
+     ('classifier', linear_model.LogisticRegression(
+         solver='newton-cg', multi_class='multinomial'))])
 
-scores = model_selection.cross_val_score(
-    estimator=pl, X=X, y=y, cv=5, n_jobs=-1)
 
-print('cross validation scores: {}'.format(scores))
-print('cross validation accuracy: {} +/- {}'
-      .format(np.mean(scores), np.std(scores)))
+scores_accuracy = model_selection.cross_val_score(
+    estimator=pl, X=X, y=y, scoring='accuracy', cv=5, n_jobs=-1)
+scores_logloss = model_selection.cross_val_score(
+    estimator=pl, X=X, y=y, scoring='neg_log_loss', cv=5, n_jobs=-1)
+
+print('\n')
+print('---------- ACCURACY ----------')
+# print('CV scores: {}'.format(scores_accuracy))
+print('Average: {0:.3f} +/- {0:.4f}'
+      .format(np.mean(scores_accuracy), np.std(scores_accuracy)))
+print('------------------------------')
+print('---------- LOG_LOSS ----------')
+# print('CV scores: {}'.format(scores_logloss))
+print('Average: {0:.3f} +/- {0:.4f}'
+      .format(np.mean(scores_logloss), np.std(scores_logloss)))
+print('------------------------------')
+
 
 pl.fit(X, y)
 
-pred_accuracy = pd.DataFrame(pl.predict(test_data))
-pred_logloss = pd.DataFrame(pl.predict_proba(test_data))
+pred_accuracy = pd.DataFrame(pl.predict(target))
+pred_logloss = pd.DataFrame(pl.predict_proba(target))
 
 out_path = os.path.join(
     os.path.abspath('..'),
