20bff00274b51b7edb5f93580a246142cd15b756
Tommi Vehviläinen <tommi.vehvilainen@aalto.fi>
2017-11-07 12:18:06 +0200

    minor changes
    the more commits you make, the more valuable you are for your organization :)

--- source/main.py
+++ source/main.py
@@ -12,44 +12,50 @@ resource_path = os.path.join(
 paths = [os.path.join(resource_path, p) for p in
          ['train_data.csv', 'train_labels.csv', 'test_data.csv']]
 
-X, y, target = [pd.read_csv(p, header=None)
+X, y, test_data = [pd.read_csv(p, header=None)
                    for p in paths]
 
 X = X.values
 y = y.values.ravel()
 
-pl = pipeline.Pipeline(
-    [('scaler', preprocessing.StandardScaler()),
-     # at the moment the pipeline performs better without
-     # PCA but it is left here for reference. For optimal performance
-     # comment the PCA line before running the code.
-     ('PCA', decomposition.PCA(n_components=0.8, svd_solver='full')),
-     ('classifier', linear_model.LogisticRegression(
-         solver='newton-cg', multi_class='multinomial'))])
+pl = pipeline.Pipeline([
+    ('scaler', preprocessing.StandardScaler()),
+    # at the moment the pipeline performs better without
+    # PCA but it is left here for reference. For optimal performance
+    # comment the PCA line before running the code.
+    ('PCA', decomposition.PCA(n_components=0.8, svd_solver='full')),
+    ('classifier', linear_model.LogisticRegression())
+])
 
 
 scores_accuracy = model_selection.cross_val_score(
     estimator=pl, X=X, y=y, scoring='accuracy', cv=5, n_jobs=-1)
-scores_logloss = model_selection.cross_val_score(
-    estimator=pl, X=X, y=y, scoring='neg_log_loss', cv=5, n_jobs=-1)
+
+# TODO: neg_log_loss seems to be negated value of log_loss
+# because sklearn requires greater value -> better fit.
+# The negation is easy to undo but also 1 must be subtracted
+# to get output that is similar to the cost given by kaggle logloss
+# scorer. Why is that?
+scores_logloss = -model_selection.cross_val_score(
+    estimator=pl, X=X, y=y, scoring='neg_log_loss', cv=5, n_jobs=-1) - 1
 
 print('\n')
 print('---------- ACCURACY ----------')
 # print('CV scores: {}'.format(scores_accuracy))
-print('Average: {0:.3f} +/- {0:.4f}'
+print('Average: {0:.3f} +/- {1:.4f}'
       .format(np.mean(scores_accuracy), np.std(scores_accuracy)))
 print('------------------------------')
 print('---------- LOG_LOSS ----------')
-# print('CV scores: {}'.format(scores_logloss))
-print('Average: {0:.3f} +/- {0:.4f}'
+print('CV scores: {}'.format(scores_logloss))
+print('Average: {0:.3f} +/- {1:.4f}'
       .format(np.mean(scores_logloss), np.std(scores_logloss)))
 print('------------------------------')
 
 
 pl.fit(X, y)
 
-pred_accuracy = pd.DataFrame(pl.predict(target))
-pred_logloss = pd.DataFrame(pl.predict_proba(target))
+pred_accuracy = pd.DataFrame(pl.predict(test_data))
+pred_logloss = pd.DataFrame(pl.predict_proba(test_data))
 
 out_path = os.path.join(
     os.path.abspath('..'),
