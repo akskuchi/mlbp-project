0b03fd4503e3ee3732a1fb92e65c366e8626b765
akskuchi <kaushiksurikuchi@gmail.com>
2017-11-23 21:42:10 +0200

    ensemble and semi-supervised models

--- source/main.py
+++ source/main.py
@@ -1,6 +1,9 @@
 import numpy as np
-from sklearn import preprocessing, linear_model, pipeline,\
+from sklearn import preprocessing, linear_model, pipeline, \
     model_selection
+from sklearn.ensemble import VotingClassifier
+from sklearn.semi_supervised import LabelSpreading
+from sklearn.svm import SVC
 
 import load_data
 import write_data
@@ -26,7 +29,37 @@ kf = model_selection.StratifiedKFold(
 
 ############### ACCURACY ###############
 
+def semi_supervised_model():
+    global X, y, kf
+    X = np.concatenate((X, test_data))
+    y = np.concatenate((y, np.repeat([0], len(test_data))))
+    print('shape of features: ', X.shape)
+    print('shape of labels: ', y.shape)
+
+    label_prop_model = LabelSpreading(kernel='rbf', n_neighbors=25, gamma=0.25)
+    label_prop_model.fit(X, y)
+
+    print('[semi-supervised] accuracy using train data: ', label_prop_model.score(X, y))
+    # TODO: submit for accuracy
+    # label_prop_model.predict(test_data)
+    # TODO: submit for log-loss
+    # label_prop_model.predict_proba(test_data)
+
+
+def ensemble_model():
+    estimators = []
+    model1 = linear_model.LogisticRegression()
+    estimators.append(('logistic', model1))
+    model2 = SVC(max_iter=-1, probability=True)
+    estimators.append(('svm', model2))
+
+    ensemble = VotingClassifier(estimators, voting='soft')
+    return ensemble
+
+
 if accuracy:
+    # using semi-supervised
+    # semi_supervised_model()
 
     gs_acc = model_selection.GridSearchCV(
         estimator=pl,
@@ -49,6 +82,10 @@ if accuracy:
 
     # use all available data to fit a model
     gs_acc.fit(X, y)
+
+    # using ensemble
+    # kfold = model_selection.KFold(n_splits=5, random_state=7)
+    # scores_accuracy = model_selection.cross_val_score(ensemble_model(), X, y, cv=kfold, scoring='accuracy')
 
     print('\n')
     print('---------- ACCURACY ----------')
@@ -87,6 +124,10 @@ if logloss:
 
     # use all available data to fit a model
     gs_log.fit(X, y)
+
+    # using ensemble
+    # kfold = model_selection.KFold(n_splits=5, random_state=7)
+    # scores_accuracy = model_selection.cross_val_score(ensemble_model(), X, y, cv=kfold, scoring='neg_log_loss')
 
     print('---------- LOG_LOSS ----------')
     print('CV scores: {}'.format(scores_logloss))
